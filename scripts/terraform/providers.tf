3d001696a852d8d1b24eb557d51b96351b4feaf5
Artur Amaral <37873236+arturamaral2@users.noreply.github.com>
2023-10-30 18:52:53 -0300

    feat(terraform): use oficial kubernetes provider (#4)

--- scripts/terraform/providers.tf
+++ scripts/terraform/providers.tf
@@ -1,14 +1,3 @@
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
-
-terraform {
-  required_version = ">= 0.13"
-
-  required_providers {
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
-    }
-  }
-}
