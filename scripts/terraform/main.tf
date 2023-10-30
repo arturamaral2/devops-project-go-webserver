2c685304ca2b18c068fbffc83da910e8e65860e9
arturamaral2 <arturamaral2008@gmail.com>
2023-10-30 18:48:44 -0300

    feat(terraform): use oficial kubernetes provider

--- scripts/terraform/main.tf
+++ scripts/terraform/main.tf
@@ -1,23 +1,23 @@
-resource "kubectl_manifest" "namespace" {
-  yaml_body = file("${path.module}/../kubernetes/namespace.yaml") 
+resource "kubernetes_manifest" "namespace" {
+  manifest = yamldecode(file("${path.module}/../kubernetes/namespace.yaml"))
 }
 
-resource "kubectl_manifest" "pv" {
-  depends_on = [kubectl_manifest.namespace]
-  yaml_body  = file("${path.module}/../kubernetes/pv.yaml") 
+resource "kubernetes_manifest" "pv" {
+  depends_on = [kubernetes_manifest.namespace]
+  manifest   = yamldecode(file("${path.module}/../kubernetes/pv.yaml"))
 }
 
-resource "kubectl_manifest" "pvc" {
-  depends_on = [kubectl_manifest.pv]
-  yaml_body  = file("${path.module}/../kubernetes/pvc.yaml")
+resource "kubernetes_manifest" "pvc" {
+  depends_on = [kubernetes_manifest.pv]
+  manifest   = yamldecode(file("${path.module}/../kubernetes/pvc.yaml"))
 }
 
-resource "kubectl_manifest" "webserver" {
-  depends_on = [kubectl_manifest.pvc]
-  yaml_body  = file("${path.module}/../kubernetes/app.yaml")
+resource "kubernetes_manifest" "webserver" {
+  depends_on = [kubernetes_manifest.pvc]
+  manifest   = yamldecode(file("${path.module}/../kubernetes/app.yaml"))
 }
 
-resource "kubectl_manifest" "service" {
-  depends_on = [kubectl_manifest.webserver]
-  yaml_body  = file("${path.module}/../kubernetes/service.yaml")
+resource "kubernetes_manifest" "service" {
+  depends_on = [kubernetes_manifest.webserver]
+  manifest   = yamldecode(file("${path.module}/../kubernetes/service.yaml"))
 }
